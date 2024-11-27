d249e63bf33c1a8cee9141892ae88f49b26a05a4
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-27 18:01:22 -0300

    First commit

---
+++ src/main/java/com/devsuperior/send_book_email/SendBookEmailApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.send_book_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendBookEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendBookEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/send_book_email/SendBookEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.send_book_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendBookEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

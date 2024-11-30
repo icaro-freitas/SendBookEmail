72fce5258cba88bde9a2ee7b2609297183d1f67d
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-30 12:15:44 -0300

    Process step

--- src/main/java/com/devsuperior/send_book_email/step/SendEmailUserStepConfig.java
+++ src/main/java/com/devsuperior/send_book_email/step/SendEmailUserStepConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.send_book_email.step;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.devsuperior.send_book_email.domain.UserBookLoan;
+import com.sendgrid.helpers.mail.Mail;
 
 @Configuration
 public class SendEmailUserStepConfig {
@@ -22,12 +24,14 @@ public class SendEmailUserStepConfig {
 	
 	@Bean
 	public Step sendEmailUserStep(
+			ItemProcessor<UserBookLoan, Mail> processLoanNotificationEmailProcessor,
 			ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader,
 			ItemWriter<UserBookLoan> sendEmailRequestReturnWriter,
 			JobRepository jobRepository) {
 		return new StepBuilder("sendEmailUserStep", jobRepository)
-				.<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
+				.<UserBookLoan, Mail>chunk(1, transactionManager)
 				.reader(readUsersWithLoansCloseToReturnReader)
+				.processor(processLoanNotificationEmailProcessor)
 				.writer(sendEmailRequestReturnWriter)
 				.build();
 	}
